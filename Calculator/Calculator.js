f6e7acbc9e31c37ce6fe3851ee42a742da10912a
Dhruv4617 <99534396+Dhruv4617@users.noreply.github.com>
2024-01-21 22:46:51 +0530

    Create Calculator.js

---
+++ Calculator/Calculator.js
@@ -0,0 +1,77 @@
+let firstOperand = "";
+let secondOperand = "";
+let currentOperator = null;
+let resetScreen = false;
+
+const display = document.querySelector("#display");
+const numButtons = document.querySelectorAll(".numButton");
+const opButtons = document.querySelectorAll(".opButton");
+const eqButton = document.querySelector(".eqButton");
+const clearButton = document.querySelector(".clearButton");
+
+numButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    if (resetScreen) {
+      display.textContent = "";
+      resetScreen = false;
+    }
+    display.textContent += button.textContent;
+  });
+});
+
+opButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    if (currentOperator !== null) compute();
+    firstOperand = display.textContent;
+    currentOperator = button.textContent;
+    display.textContent = `${firstOperand} ${currentOperator}`;
+    resetScreen = true;
+  });
+});
+
+eqButton.addEventListener('click', () => {
+  if (currentOperator === null) return;
+  secondOperand = display.textContent.split(" ")[0];
+  compute();
+});
+
+clearButton.addEventListener('click', () => {
+  display.textContent = "0";
+  firstOperand = "";
+  secondOperand = "";
+  currentOperator = null;
+});
+
+function compute() {
+  if (currentOperator === null) return;
+
+  secondOperand = display.textContent.split(" ")[0];
+  const result = operate(currentOperator, firstOperand, secondOperand);
+  display.textContent = `${firstOperand} ${currentOperator} ${secondOperand} = ${result}`;
+  currentOperator = null;
+}
+
+function clear() {
+  display.textContent = "0";
+  firstOperand = "";
+  secondOperand = "";
+  currentOperator = null;
+}
+
+function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
+  switch (operator) {
+    case '+':
+      return a + b;
+    case '-':
+      return a - b;
+    case '*':
+      return a * b;
+    case '/':
+      if (b === 0) return 'Error! Div by 0';
+      return a / b;
+    default:
+      return b;
+  }
+}
